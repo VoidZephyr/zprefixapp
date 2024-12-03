231acb045f07016c722d45b6009b2cdc16ca5324
VoidZephyr <jking21412@gmail.com>
2024-12-03 11:52:58 -0800

    its working! front and back

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,33 +1,34 @@
 import React, {useEffect, useState} from 'react';
 import './App.css'
 
-function App() {
-  const [items, setItems] = useState([])
+const App = () => {
+  const [items, setItems] = useState([]);
 
+  useEffect(() => {
+  fetch("http://localhost:5001/items")
+  .then((res) => res.json())
+  .then((data) => setItems(data))
+}, [])
+
+  // useEffect(() => {   https://dev.to/antdp425/react-fetch-data-from-api-with-useeffect-27le
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Inventory Website SupraCoders</h1>
+      <h2>Items</h2>
+      <ul>
+        {items.length > 0 ? (
+          items.map((item) => (
+            <li key={item.id}>
+              {item.name} - {item.description} - (Quantity: {item.quantity})
+            </li>
+          ))
+        ) : (
+          <li>No items available.</li>
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default App
