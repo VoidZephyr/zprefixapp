9b24d91a70cb755309c731bc25517b7f0177a92f
VoidZephyr <jking21412@gmail.com>
2024-12-03 09:56:35 -0800

    app.getr

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -8,9 +8,24 @@ dotenv.config();
 app.use(express.json());
 //https://stackoverflow.com/questions/18864677/what-is-process-env-port-in-node-js
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+//needs items to display https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+
+const items = [
+{id : 1, itemName: 'Item1', description: "blablabal", quantity: 22 },
+{id : 2, itemName: 'Item2', description: "asdfasdfasdfa", quantity: 3}
+]
+//https://expressjs.com/en/guide/routing.html
+app.get('/items', function (req, res) {
+    res.send(items);
   });
+
+
+
+
+
+
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is on ${PORT}`));
