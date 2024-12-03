41e71ac31f6272195a16f372242e492c4fe788d3
VoidZephyr <jking21412@gmail.com>
2024-12-03 12:25:43 -0800

    computer restart

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -5,9 +5,10 @@ const App = () => {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-  fetch("http://localhost:5001/items")
+  fetch('http://localhost:5001/items')
   .then((res) => res.json())
   .then((data) => setItems(data))
+  .catch((err) => console.error('Error fetching items:', err)); //adding a .catch to troubleshoot
 }, [])
 
   // useEffect(() => {   https://dev.to/antdp425/react-fetch-data-from-api-with-useeffect-27le
