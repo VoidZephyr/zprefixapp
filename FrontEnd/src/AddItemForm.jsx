23a6862aec5ffcc686e9e776856669cde2fbe367
VoidZephyr <jking21412@gmail.com>
2024-12-03 21:19:53 -0800

    beginning additemform

--- FrontEnd/src/AddItemForm.jsx
+++ FrontEnd/src/AddItemForm.jsx
@@ -1,6 +1,19 @@
 import React, {useEffect, useState} from 'react';
 
+const AddItemForm =({onItemAdded}) => {
+    const [formData, setFormData] = useState(
+        {
+            name: "",
+            description: "",
+            quantity: 0,
+            user_id: 1,
+        }
+    );
 
+
+
+    
+}
 
 
 <form>
