e4ce3f94cdd4e85e4c9e2f4bf41add174004a4b6
VoidZephyr <jking21412@gmail.com>
2024-12-03 22:29:35 -0800

    beginning additemform

--- FrontEnd/src/AddItemForm.jsx
+++ FrontEnd/src/AddItemForm.jsx
@@ -10,10 +10,30 @@ const AddItemForm =({onItemAdded}) => {
         }
     );
 
+    //according to the internet this is better than using onclick event for react app
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
 
-    
-}
+        fetch('http://localhost:5000/items', {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json',
+            },
+        body: JSON.stringify(formData),
+        })
+        .then((res) => {
+            if (!res.ok) {
+            throw new Error('Failed to create item');
+            }
+            return res.json();
+        })
+        .then((newItem) => {
+            onItemAdded(newItem); // Notify parent about the new item
+            setFormData({ name: '', description: '', quantity: 0, user_id: 1 }); // Reset form
+        })
+        .catch((err) => console.error('Error adding item:', err));
+    };
 
 
 <form>
